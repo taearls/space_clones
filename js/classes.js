feef99757c9d10262269addcbaa2c6fbe936a767
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-30 11:27:26 -0500

    added html and css file for title screen, set up basic elements

---
+++ js/classes.js
@@ -0,0 +1,48 @@
+// initialize generic Ship class
+class Ship {
+	constructor() {
+		this.name = name;
+		this.firepower = firepower;
+		this.shield = shield;
+	}
+	fire() {
+		//
+	}
+	move() {
+		// for enemies, increment speed with levels
+	}
+	die() {
+		// some sort of explosion graphic overlaying the ship sprite
+		// update enemies remaining
+		// update player lives
+	}
+}
+
+//
+class Player extends Ship {
+	constructor() {
+		super(firepower, shield)
+		this.name = input.val();
+		this.playerScore = 0;
+	}
+	score() {
+		// define parameters to increment playerScore
+	}
+	move() {
+		// define keydowns for left and right arrows, control of it
+	}
+}
+
+class Alien extends Ship {
+	constructor() {
+		super(name, firepower, shield);
+		this.speed = speed;
+	}
+}
+
+class Mothership extends Ship {
+	constructor() {
+		super(name, firepower, shield);
+		this.speed = speed;
+	}
+}
