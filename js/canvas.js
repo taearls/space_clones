219177925dc21377feef47e44e8fe94aee6313ab
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-31 18:07:28 -0500

    fixed window resize issue with canvas

--- js/canvas.js
+++ js/canvas.js
@@ -7,29 +7,32 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 // create star array to store new stars
-const stars = [];
+let stars = [];
 
-// generate 500 stars, push into star array
-for (i = 0; i < 500; i++) {
-	let x = Math.random() * canvas.width;
-	let y = Math.random() * canvas.height;
-	let radius = Math.ceil(Math.random() * 2);
-	let dy = Math.random() * (5);
-	ctx.beginPath();
-	ctx.fillStyle = "white";
-	ctx.arc(x, y, radius, 0, Math.PI * 2);
-	ctx.fill();
-	stars.push(new Star(x, y, dy, radius))
+// generate 500 stars with function, push into star array
+const initStars = () => {
+	stars = [];
+	for (i = 0; i < 100; i++) {
+		let x = Math.random() * canvas.width;
+		let y = Math.random() * canvas.height;
+		let radius = Math.ceil(Math.random() * 2);
+		let dy = Math.random() * 1;
+		ctx.beginPath();
+		ctx.fillStyle = "white";
+		ctx.arc(x, y, radius, 0, Math.PI * 2);
+		ctx.fill();
+		stars.push(new Star(x, y, dy, radius))
+	}
 }
-
+initStars();
 // animate the stars in canvas backdrop
 function animateCanvas() {
 	requestAnimationFrame(animateCanvas);
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	for (let i = 0; i < stars.length; i++) {
 		stars[i].draw();
-		stars[i].move();
 		stars[i].update();
+		stars[i].move();
 	}
 }
 
@@ -43,11 +46,14 @@ const getDistance = (x1, y1, x2, y2) => {
 	// use pythagoreon theorum to calc distance
 	return Math.sqrt(xDistance ** 2 + yDistance ** 2);
 }
-
 // ***** EVENT LISTENERS *****
 
-document.addEventListener("resize", function(event) {
+// for some reason, stars speed up exponentially when window resizes, but they do regenerate as desired
+window.addEventListener("resize", function(event) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	initStars();
 	animateCanvas();
 })
 
